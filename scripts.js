5be859089c53874932d05e6d91fdb237fb7c73d2
Hossein Simchi <95399810+HosseinSimchi@users.noreply.github.com>
2022-06-04 10:36:49 +0430

    the second try is successful

--- scripts.js
+++ scripts.js
@@ -1,16 +1,22 @@
-
 function newElement(){
     var mytext = document.getElementById("myInput").value;
     let CreateLiElement = document.createElement("li");
     var myULList = document.getElementsByTagName("UL")[0];
     CreateLiElement.textContent = mytext;
-    CreateLiElement.className = "checked"
-    function DoSomething(){
-        console.log("Hi");
-    }
-    CreateLiElement.onclick = DoSomething();
     myULList.appendChild(CreateLiElement);
-   
 }
 
+window.onload=function(){
+    var list = document.querySelector('ul');
+    list.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }}, false);
+}
 
+function RefreshPage() {
+    var list = document.getElementsByTagName('LI');
+    for (var i = 0; i < list.length; i++) {
+        list[i].style.display = 'none'
+    }
+}
