dd090b22ad0161b237eb0b5090158ef7434cca3f
Hossein Simchi <95399810+HosseinSimchi@users.noreply.github.com>
2022-06-04 18:24:43 +0430

    it's finished

--- scripts.js
+++ scripts.js
@@ -1,18 +1,27 @@
+let array = [];
 function newElement(){
+    randomNumber = Math.floor((Math.random() * 100) + 1);
+    array.push(randomNumber);
     var mytext = document.getElementById("myInput").value;
+    localStorage.setItem("myinput" + randomNumber, mytext)
+    var getmytext = localStorage.getItem("myinput" + randomNumber);
     let CreateLiElement = document.createElement("li");
     var myULList = document.getElementsByTagName("UL")[0];
-    CreateLiElement.textContent = mytext;
+    CreateLiElement.textContent = getmytext;
     myULList.appendChild(CreateLiElement);
 }
 
 window.onload=function(){
     var list = document.querySelector('ul');
-    list.addEventListener('click', function(ev) {
+        list.addEventListener('click', function(ev) {
     if (ev.target.tagName === 'LI') {
         ev.target.classList.toggle('checked');
+        
     }}, false);
+
 }
+
+
 
 function RefreshPage() {
     var list = document.getElementsByTagName('LI');
